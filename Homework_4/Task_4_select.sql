a43142ad1874a812881a8b061d510624cd812692
EvgenyMyznikov <evgenymyz@gmail.com>
2020-11-30 17:20:22 +0300

    "modified Homework_4/Task_4.sql"

--- Homework_4/Task_4_select.sql
+++ Homework_4/Task_4_select.sql
@@ -1,10 +1,9 @@
 SELECT name, release_year
 FROM album
-WHERE release_year IN('2018-01-01', '2018-12-31');
+WHERE release_year IN ('2018-01-01', '2018-12-31');
 
-SELECT name, duration
-FROM track
-WHERE duration = (SELECT MAX(duration) FROM track);
+SELECT name, MAX(duration)
+FROM track;
 
 SELECT name
 FROM track
@@ -12,10 +11,10 @@ WHERE duration >= '03:30';
 
 SELECT name
 FROM collection
-WHERE release_year IN('2018-01-01', '2020-12-31')
+WHERE release_year IN ('2018-01-01', '2020-12-31');
 
 SELECT name
-FROM artist 
+FROM artist
 WHERE name NOT LIKE '% %';
 
 SELECT name
