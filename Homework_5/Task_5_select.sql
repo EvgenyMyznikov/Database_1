007cfad7c1082d97ac302423b31cf6e35da31f42
EvgenyMyznikov <evgenymyz@gmail.com>
2020-12-10 15:00:42 +0300

    "modified: Homework_5/Task_5_select.sql"

--- Homework_5/Task_5_select.sql
+++ Homework_5/Task_5_select.sql
@@ -13,54 +13,44 @@ SELECT albumid, AVG(duration) FROM track
 GROUP BY albumid;
 
 -- 4.все исполнители, которые не выпустили альбомы в 2020 году;
-SELECT name FROM artist
-WHERE id IN(
-    SELECT artist_id FROM albumartist
-    WHERE album_id IN(
-        SELECT id FROM album
-        WHERE release_year < '2020-01-01'));
+SELECT DISTINCT a.name FROM artist AS a
+LEFT JOIN albumartist AS aa ON a.id = aa.id
+LEFT JOIN album AS am ON aa.id = am.id
+WHERE release_year < '2020-01-01';
 
 -- 5.названия сборников, в которых присутствует конкретный исполнитель (выберите сами);
-SELECT name FROM collection
-WHERE id IN(
-    SELECT track_id FROM collectiontrack
-    WHERE track_id IN(
-        SELECT id FROM track
-        WHERE id IN(
-            SELECT artist_id FROM albumartist
-            WHERE artist_id IN(
-                SELECT id FROM artist
-                WHERE id = 3))));
+SELECT DISTINCT c.name FROM collection AS c
+LEFT JOIN artist AS a ON c.id = a.id
+WHERE a.id = 3;
 
 -- 6.название альбомов, в которых присутствуют исполнители более 1 жанра;
-SELECT name FROM album
-WHERE id IN(
-    SELECT artist_id FROM albumartist
-    WHERE artist_id IN(
-        SELECT artist_id FROM genreartist
-        GROUP BY artist_id
-        HAVING COUNT(genre_id) > 1));
+SELECT DISTINCT a.name FROM album AS a
+LEFT JOIN albumartist AS aa ON a.id = aa.id
+LEFT JOIN genreartist AS ga ON aa.id = ga.artist_id
+GROUP BY a.name
+HAVING COUNT(ga.artist_id) > 1;
 
 -- 7.наименование треков, которые не входят в сборники;
-SELECT name FROM track
+SELECT t.name FROM track AS t
 WHERE id NOT IN(
-    SELECT track_id FROM collectiontrack
-    GROUP BY track_id);
+    SELECT ct.track_id FROM collectiontrack AS ct
+	LEFT JOIN collection AS c ON ct.id = c.id
+    GROUP BY ct.track_id);
 
 -- 8.исполнителя(-ей), написавшего самый короткий по продолжительности трек;
-SELECT name FROM artist
-WHERE id in(
-    SELECT artist_id FROM albumartist
-    WHERE album_id = (
-        SELECT albumid FROM track
+SELECT a.name FROM artist AS a
+WHERE id IN(
+    SELECT aa.artist_id FROM albumartist AS aa
+    LEFT JOIN track AS t ON aa.id = t.albumid
         WHERE duration IN(
-            SELECT MIN(duration) FROM track)));
+            SELECT MIN(duration) FROM track));
 
 -- 9.название альбомов, содержащих наименьшее количество треков.
-SELECT name FROM album
-WHERE id in(
-    SELECT COUNT(albumid) FROM track
-    GROUP BY albumid
-    ORDER BY albumid DESC
-    LIMIT 1);
+SELECT DISTINCT a.name FROM album AS a
+INNER JOIN track AS t ON a.id = t.albumid
+WHERE albumid IN(
+	SELECT COUNT(albumid) FROM track
+	GROUP BY albumid
+	ORDER BY albumid
+	LIMIT 1);
 
